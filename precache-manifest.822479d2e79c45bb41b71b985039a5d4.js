8f6538f5893829ded9765dc816ec9d955fafd144
shakil <shakhout@alfadocs.com>
2020-05-29 10:26:22 +0200

    Updates

---
+++ precache-manifest.822479d2e79c45bb41b71b985039a5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9433d467ff928e1638f3d1ba6b48841",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c0ad4bc2f5c94b86253",
+    "url": "/monsters-rolodex/static/css/main.0f6ac557.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0ad4bc2f5c94b86253",
+    "url": "/monsters-rolodex/static/js/main.eb4002f2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
